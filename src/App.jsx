b51a30ddedacd4e40ebca8627975c9de768348dd
Mamun13 <mamuncse82@gmail.com>
2022-07-05 12:35:37 +0600

    Environment_setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Incre from "./Incre";
+import Accordion from "./Accordion";
+
+const App = () =>{
+   return(
+      <>
+         <Accordion />
+      </>
+   )
+};
+export default App;
